e53de243e41238f2d5dbb290680c911797c2ba73
在隆 田 <m15763795075_1@163.com>
2020-09-15 00:05:07 +0800

    第一次提交

---
+++ Homework1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入一个正整数!");
+            int num = Convert.ToInt32(Console.ReadLine());
+            List<int> list = GetFactors(num);
+            Console.Write("素因子有:");
+            foreach (int i in list)
+            {
+                Console.Write("\t" + i);
+            }
+        }
+        private static List<int> GetFactors(int num)
+        {
+            List<int> list = new List<int>();
+            for(int i=2;i*i<num;i++)
+                if(num % i == 0)
+                {
+                    list.Add(i);
+                    num = num / i;
+                }
+
+            if (num != 1) { list.Add(num); }
+
+            return list;
+        }
+
+    }
+}
