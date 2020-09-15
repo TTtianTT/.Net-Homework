ba1292f5cbc80aebe54c076df14b7c6aa76f6819
在隆 田 <m15763795075_1@163.com>
2020-09-15 11:47:36 +0800

    第一次作业

---
+++ HW1/HW1.3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> primes = new List<int>();
+            primes = GetPrime();
+            foreach (int num in primes)
+            {
+                Console.WriteLine(num);
+            }
+        }
+        private static List<int> GetPrime()
+        {
+            List<int> primes = new List<int>();
+            for (int i = 1; i <= 100; i++)//初始化List
+                primes.Add(i);
+
+
+            for (int i = 1; i < 101; i++)
+            {
+                for (int j = 2; j * j <= i; j++)
+                {
+                    if (i % j == 0 && i / j != 1)
+                    {
+                        primes.Remove(i);//是某个数倍数的去掉
+                    }
+                }
+            }
+            return primes;
+        }
+
+    }
+}
+
---
+++ HW1/HW1.4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace HW1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = { { 5, 2, 3, 4 }, { 5, 1, 2, 3 }, { 9, 5, 1, 2 } };
+            Console.WriteLine($"result:{IsToeplitzMatrix(matrix)}");
+        }
+        private static bool IsToeplitzMatrix(int[,] matrix)
+        {
+            bool flag = true;
+            for (int i = 1; i < matrix.GetLength(0); i++)
+                for (int j = 1; j < matrix.GetLength(1); j++)
+                    if (matrix[i, j] != matrix[i - 1, j - 1])
+                    {
+                        flag = false;
+                    }
+            return flag;
+        }
+    }
+}
+
